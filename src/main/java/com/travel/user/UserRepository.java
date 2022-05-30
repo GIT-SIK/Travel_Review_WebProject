86e6a4f7eac5dcb8ef5955e693a0d5cb49e56c97
GIT-SIK <xeno_s@naver.com>
2022-05-30 19:08:56 +0900

    Repository :  클래스 -> 인터페이스로 변경

--- src/main/java/com/travel/user/UserRepository.java
+++ src/main/java/com/travel/user/UserRepository.java
@@ -1,7 +1,10 @@
 package com.travel.user;
 
+import com.travel.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
