763a396e67c35981c0dc9435fce91ba9aa071506
YangHun Jung <narwhal1123@gmail.com>
2022-05-31 11:58:36 +0900

    findpw

--- src/main/java/com/travel/find/FindController.java
+++ src/main/java/com/travel/find/FindController.java
@@ -1,8 +1,41 @@
 package com.travel.find;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/find")
 public class FindController {
 
+    private final FindService findService;
+
+    @GetMapping("/findId")
+    public String findId() {
+        return "view/find/find-id";
+    }
+
+    @PostMapping("/findIdByTel")
+    public String findIdByTel(@RequestParam("tel") String tel, Model model) {
+        String message = findService.findIdByTel(tel);
+        model.addAttribute("message", message);
+        return "view/find/find-id-result";
+    }
+
+    @PostMapping("/findIdByEmail")
+    public String findIdByEmail(@RequestParam("email") String email, Model model) {
+        String message = findService.findIdByEmail(email);
+        model.addAttribute("message", message);
+        return "view/find/find-id-result";
+    }
+
+    @GetMapping("/findPw")
+    public String findPwAuth() {
+        return "view/find/find-pw";
+    }
 }
--- src/main/java/com/travel/find/FindService.java
+++ src/main/java/com/travel/find/FindService.java
@@ -1,7 +1,37 @@
 package com.travel.find;
 
+import com.travel.domain.User;
+import com.travel.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FindService {
+
+    private final UserRepository userRepository;
+
+    // 전화번호를 통해 유저 정보 찾기
+    public String findIdByTel(String tel) {
+        User user = userRepository.findByTel(tel);
+        String message;
+        if (user == null) {
+            message = "회원 정보를 찾을 수 없습니다.";
+        } else {
+            message = "회원님의 ID는 [" + user.getId() + "] 입니다";
+        }
+        return message;
+    }
+
+    // 메일 주소를 통해 유저 정보 찾기
+    public String findIdByEmail(String email) {
+        User user = userRepository.findByEmail(email);
+        String message;
+        if (user == null) {
+            message = "회원 정보를 찾을 수 없습니다.";
+        } else {
+            message = "회원님의 ID는 [" + user.getId() + "] 입니다";
+        }
+        return message;
+    }
 }
