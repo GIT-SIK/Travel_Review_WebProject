0619fcb16beff59fcdfc029d9f00d3124122ec67
YangHun Jung <narwhal1123@gmail.com>
2022-06-02 17:07:29 +0900

    data call

--- src/main/java/com/travel/festival/FestivalService.java
+++ src/main/java/com/travel/festival/FestivalService.java
@@ -1,7 +1,24 @@
 package com.travel.festival;
 
+import com.travel.domain.Festival;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class FestivalService {
+
+    private final FestivalRepository festivalRepository;
+
+    public List<Festival> festivalData(LocalDate month) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        String date = formatter.format(month);
+        List<Festival> result = festivalRepository.findByDate(date);
+        return result;
+    }
 }
