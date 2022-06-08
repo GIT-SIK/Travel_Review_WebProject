3595543844d9babece893712d72c910696858671
GIT-SIK <xeno_s@naver.com>
2022-06-08 10:51:43 +0900

    전체적인 타이틀 통합 / 일부 CSS / 필요없는 코드 제거

--- src/main/java/com/travel/security/config/SecurityConfig.java
+++ src/main/java/com/travel/security/config/SecurityConfig.java
@@ -30,21 +30,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .maxSessionsPreventsLogin(false);
 
         http.authorizeRequests()
-//              .antMatchers("/user/mypage").authenticated() // 로그인할 경우에만 진입가능한 경로 (마이페이지 없기 때문에 주석처리)
-                .antMatchers("/user/admin/**").access("hasRole('ROLE_ADMIN')") // admin으로 시작하는 주소는 ROLE_ADMIN만 접근가능
+                .antMatchers("/user/admin/**").access("hasRole('ROLE_ADMIN')") // user/admin으로 시작하는 주소는 ROLE_ADMIN만 접근가능
                 .antMatchers("/board/write").authenticated()//로그인시 후기작성
                 .antMatchers("/user/mypage/**").authenticated()// mypage 접근 권한
                 .anyRequest().permitAll() // 모든 사람이 접근할 수 있다.
               .and()
                 .formLogin()
                 .loginPage("/login") // 로그인페이지 설정
-                .loginProcessingUrl("/login") // login주소가 호출시 시큐리티가 낚아채서 CustomUserDetails 로 이동
+                .loginProcessingUrl("/login") // login주소가 호출시 시큐리티가 낚아채서 UserDetails 로 이동
                 .defaultSuccessUrl("/") //로그인 성공 후
                 .failureHandler(loginFailHandler()) // 로그인 실패시 핸들러로 오류 메시지 출력
               .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/") // 로그아웃경우 기본경로 위치설정
+                .logoutSuccessUrl("/") // 로그아웃 경우 기본경로 위치설정
                 .deleteCookies("JSESSIONID")
                 .invalidateHttpSession(true);
 
