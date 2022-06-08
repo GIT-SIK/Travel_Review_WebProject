bb30817ac7ea8e3fe4ae07cb22b918c631f63121
Sam <vkdnsxkdlrj@gmail.com>
2022-06-08 11:34:52 +0900

    게시글 작성 시 지도 추가

---
+++ src/main/java/com/travel/TomcatWebCustomConfig.java
@@ -0,0 +1,17 @@
+package com.travel;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatWebCustomConfig implements
+    WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+  @Override
+  public void customize(TomcatServletWebServerFactory factory) {
+    factory.addConnectorCustomizers((TomcatConnectorCustomizer)
+        connector -> connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}"));
+  }
+}
